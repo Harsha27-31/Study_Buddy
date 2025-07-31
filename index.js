f8e6a052129f5606c31a89cbfa1c3d687ff4e358
RAPAHA HARSHA VARDHAN <rapahaharshavardhan@RAPAHAs-MacBook-Air.local>
2025-07-31 20:58:31 +0530

    Initial commit

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ const upload = multer({
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-app.use('/upload', express.static(path.join(__dirname, 'public', 'upload')));
+app.use('/upload', express.static(uploadDir));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
